2b83918471633c040577e48a234eb2b95033e784
junmo95 <junmop950@gmail.com>
2023-10-22 16:04:10 +0900

    Feat: 프로젝트 골조 생성

---
+++ src/main/java/toy/five/triprecord/TripRecordApplication.java
@@ -0,0 +1,13 @@
+package toy.five.triprecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripRecordApplication.class, args);
+	}
+
+}
