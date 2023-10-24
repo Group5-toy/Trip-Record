efee03296292e02cc409105a6d72c64fda781d4d
junmo95 <junmop950@gmail.com>
2023-10-24 19:03:47 +0900

    Refactor: 엔티티 구조 변화와 폴더 정리에 따른 수정 사항들

--- src/main/java/toy/five/triprecord/domain/jouney/repository/JourneyInterface.java
+++ src/main/java/toy/five/triprecord/domain/jouney/repository/JourneyInterface.java
@@ -1,7 +1,7 @@
 package toy.five.triprecord.domain.jouney.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import toy.five.triprecord.domain.jouney.entity.Journey;
+import toy.five.triprecord.domain.jouney.entity.MoveJourney;
 
-public interface JourneyInterface extends JpaRepository<Journey, Long> {
+public interface JourneyInterface extends JpaRepository<MoveJourney, Long> {
 }
