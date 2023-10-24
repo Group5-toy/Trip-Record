56e355e67a96851fb24daec29c5c2fc1d87705c9
junmo95 <junmop950@gmail.com>
2023-10-24 19:02:25 +0900

    Feat: 여행(Trip) 엔티티 구현

--- src/main/java/toy/five/triprecord/domain/trip/entity/Trip.java
+++ src/main/java/toy/five/triprecord/domain/trip/entity/Trip.java
@@ -1,14 +1,17 @@
 package toy.five.triprecord.domain.trip.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import toy.five.triprecord.domain.jouney.entity.LodgmentJourney;
+import toy.five.triprecord.domain.jouney.entity.MoveJourney;
+import toy.five.triprecord.domain.jouney.entity.VisitJourney;
 import toy.five.triprecord.global.common.BaseTimeEntity;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
@@ -20,5 +23,25 @@ public class Trip extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 30)
+    private String name;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "trip", cascade = CascadeType.ALL)
+    private List<MoveJourney> moveJourneys;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "trip", cascade = CascadeType.ALL)
+    private List<VisitJourney> visitJourneys;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "trip", cascade = CascadeType.ALL)
+    private List<LodgmentJourney> lodgmentJourneys;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    private Boolean isDomestic;
+
+
 
 }
