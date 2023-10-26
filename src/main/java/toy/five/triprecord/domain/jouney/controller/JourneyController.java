1a859a75eb6ab6b3a574cd7543b987e3a3bb6b54
junmo95 <junmop950@gmail.com>
2023-10-26 17:18:04 +0900

    Feat: Journey update 관련 컨트롤러 로직 추가 - 기존 방식에 바꿈

--- src/main/java/toy/five/triprecord/domain/jouney/controller/JourneyController.java
+++ src/main/java/toy/five/triprecord/domain/jouney/controller/JourneyController.java
@@ -6,7 +6,13 @@ import org.springframework.web.bind.annotation.*;
 import toy.five.triprecord.domain.jouney.dto.journey_create.request.JourneyCreateRequest;
 import toy.five.triprecord.domain.jouney.dto.journey_create.response.JourneyCreateResponse;
 import toy.five.triprecord.domain.jouney.dto.journey_update.request.JourneyUpdateRequest;
+import toy.five.triprecord.domain.jouney.dto.journey_update.request.LodgmentJourneyUpdateRequest;
+import toy.five.triprecord.domain.jouney.dto.journey_update.request.MoveJourneyUpdateRequest;
+import toy.five.triprecord.domain.jouney.dto.journey_update.request.VisitJourneyUpdateRequest;
 import toy.five.triprecord.domain.jouney.dto.journey_update.response.JourneyUpdateResponse;
+import toy.five.triprecord.domain.jouney.dto.journey_update.response.LodgmentJourneyUpdateResponse;
+import toy.five.triprecord.domain.jouney.dto.journey_update.response.MoveJourneyUpdateResponse;
+import toy.five.triprecord.domain.jouney.dto.journey_update.response.VisitJourneyUpdateResponse;
 import toy.five.triprecord.domain.jouney.service.JourneyService;
 
 @RequiredArgsConstructor
@@ -22,13 +28,37 @@ public class JourneyController {
     }
 
     @PostMapping("/{tripId}")
-    public JourneyCreateResponse createJourney(@PathVariable Long tripId, @RequestBody JourneyCreateRequest request) {
+    public JourneyCreateResponse createJourney(
+            @PathVariable Long tripId,
+            @RequestBody JourneyCreateRequest request
+    ) {
         return journeyService.saveJourneys(tripId, request);
     }
 
-    @PutMapping("/{journeyId}/update")
-    public JourneyUpdateResponse updateJourney(@PathVariable Long journeyId, @RequestBody JourneyUpdateRequest request) {
-        return journeyService.modifyJourney(journeyId, request);
+    @PutMapping("/move/{journeyId}")
+    public MoveJourneyUpdateResponse updateMoveJourney(
+            @PathVariable Long journeyId,
+            @RequestBody MoveJourneyUpdateRequest updateRequest
+    ) {
+        return journeyService.modifyMoveJourney(journeyId, updateRequest);
     }
+
+    @PutMapping("/lodgment/{journeyId}")
+    public LodgmentJourneyUpdateResponse updateLodgmentJourney(
+            @PathVariable Long journeyId,
+            @RequestBody LodgmentJourneyUpdateRequest updateRequest
+    ) {
+        return journeyService.modifyLodgmentJourney(journeyId, updateRequest);
+    }
+
+    @PutMapping("/visit/{journeyId}")
+    public VisitJourneyUpdateResponse updateVisitJourney(
+            @PathVariable Long journeyId,
+            @RequestBody VisitJourneyUpdateRequest updateRequest
+    ) {
+        return journeyService.modifyVisitJourney(journeyId, updateRequest);
+    }
+
+
 
 }
