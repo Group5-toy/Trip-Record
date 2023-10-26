563744295f488072220ea4950e71dac9c3ab91b5
parkgeonwoo <pgw111111@naver.com>
2023-10-26 12:27:24 +0900

    Refactor:오류 코드 추가
    - 여행에 대한 POST,PUT 오류코드 추가

--- src/main/java/toy/five/triprecord/global/exception/ErrorCode.java
+++ src/main/java/toy/five/triprecord/global/exception/ErrorCode.java
@@ -8,10 +8,15 @@ import lombok.Getter;
 public enum ErrorCode {
 
     //Trip
-    TRIP_NO_EXIST("해당 ID의 여행 정보가 없습니다."),
+    TRIP_NO_EXIST(400,"해당 ID의 여행 정보가 없습니다."),
+    TRIP_INVALID_TIME(400,"시간이 올바르게 입력되지 않았습니다."),
+    TRIP_INVALID_PARAMETER(400,"입력하신 여행의 정보가 전부 입력되지 않았습니다."),
+    TRIP_IVALID_UPDATE(400,"업데이트하고자 하는 여행의 정보가 한 개는 필요합니다."),
     ;
 
-    //J
+    //Journey
 
+    private final int statusCode;
     private final String message;
+
 }
