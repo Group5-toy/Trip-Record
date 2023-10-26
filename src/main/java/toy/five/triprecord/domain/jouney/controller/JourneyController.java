ead781639171b39571eb9b66832d0425c50674ec
junmo95 <junmop950@gmail.com>
2023-10-26 13:31:12 +0900

    Feat: 여정 update 컨트롤러 추가

--- src/main/java/toy/five/triprecord/domain/jouney/controller/JourneyController.java
+++ src/main/java/toy/five/triprecord/domain/jouney/controller/JourneyController.java
@@ -3,8 +3,10 @@ package toy.five.triprecord.domain.jouney.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import toy.five.triprecord.domain.jouney.dto.request.JourneyRequest;
-import toy.five.triprecord.domain.jouney.dto.response.JourneyResponse;
+import toy.five.triprecord.domain.jouney.dto.journey_create.request.JourneyCreateRequest;
+import toy.five.triprecord.domain.jouney.dto.journey_create.response.JourneyCreateResponse;
+import toy.five.triprecord.domain.jouney.dto.journey_update.request.JourneyUpdateRequest;
+import toy.five.triprecord.domain.jouney.dto.journey_update.response.JourneyUpdateResponse;
 import toy.five.triprecord.domain.jouney.service.JourneyService;
 
 @RequiredArgsConstructor
@@ -20,8 +22,13 @@ public class JourneyController {
     }
 
     @PostMapping("/{tripId}")
-    public JourneyResponse createJourney(@PathVariable Long tripId, @RequestBody JourneyRequest request) {
+    public JourneyCreateResponse createJourney(@PathVariable Long tripId, @RequestBody JourneyCreateRequest request) {
         return journeyService.saveJourneys(tripId, request);
+    }
+
+    @PutMapping("/{journeyId}/update")
+    public JourneyUpdateResponse updateJourney(@PathVariable Long journeyId, @RequestBody JourneyUpdateRequest request) {
+        return journeyService.modifyJourney(journeyId, request);
     }
 
 }
