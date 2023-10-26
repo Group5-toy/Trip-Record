722533a744f2f0119d902bd9ffd55fa2fabb31dc
parkgeonwoo <pgw111111@naver.com>
2023-10-26 12:28:03 +0900

    Feat:GlobalExceptionHandler 구현
    -BaseException이 발생하면 해당 예외를 처리해줄 Handler 클래스 구현

---
+++ src/main/java/toy/five/triprecord/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package toy.five.triprecord.global.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ApiResponse<String>> handleBaseException(BaseException e) {
+        return new ResponseEntity<>(ApiResponse.fail(e.getStatusCode(), e.getMessage()), HttpStatus.valueOf(e.getStatusCode()));
+    }
+}
