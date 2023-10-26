1569634c905c08bab5bec9d83a15b9caf16d0995
parkgeonwoo <pgw111111@naver.com>
2023-10-27 01:13:24 +0900

    Refactor:기본 메시지 생성
    -@Valid 진행 중 발생하는 기본 오류 메시지 설정

--- src/main/java/toy/five/triprecord/domain/trip/dto/request/TripUpdateRequest.java
+++ src/main/java/toy/five/triprecord/domain/trip/dto/request/TripUpdateRequest.java
@@ -3,10 +3,14 @@ package toy.five.triprecord.domain.trip.dto.request;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import toy.five.triprecord.domain.trip.validation.updatetime.TripUpdateTimeConstraint;
 
 import java.time.LocalDateTime;
 
@@ -15,25 +19,24 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@TripUpdateTimeConstraint
 public class TripUpdateRequest {
+
+    @NotNull(message ="필드가 전부 채워져야 합니다.")
+    @NotEmpty(message ="필드가 전부 채워져야 합니다.")
+    @NotBlank(message ="필드가 전부 채워져야 합니다.")
     private String name;
+
+    @NotNull(message ="필드가 전부 채워져야 합니다.")
     private LocalDateTime startTime;
+
+    @NotNull(message ="필드가 전부 채워져야 합니다.")
     private LocalDateTime endTime;
+
+    @NotNull(message ="필드가 전부 채워져야 합니다.")
     private Boolean isDomestic;
 
-    public void updateFromTripCreateRequest(TripUpdateRequest tripUpdateRequest) {
-        if (!tripUpdateRequest.getName().isEmpty()) {
-            this.name = tripUpdateRequest.getName();
-        }
-        if (tripUpdateRequest.getStartTime() != null) {
-            this.startTime = tripUpdateRequest.getStartTime();
-        }
-        if (tripUpdateRequest.getEndTime() != null) {
-            this.endTime = tripUpdateRequest.getEndTime();
-        }
-        if (tripUpdateRequest.getIsDomestic() != null) {
-            this.isDomestic = tripUpdateRequest.getIsDomestic();
-        }
-    }
+
+
 
 }
