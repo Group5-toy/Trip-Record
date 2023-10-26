d7a738fb2b63a3e234b175d6dd7dd016060bba59
parkgeonwoo <pgw111111@naver.com>
2023-10-27 01:13:06 +0900

    Feat:PATCH DTO 생성

---
+++ src/main/java/toy/five/triprecord/domain/trip/dto/request/TripPatchRequest.java
@@ -0,0 +1,48 @@
+package toy.five.triprecord.domain.trip.dto.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import toy.five.triprecord.domain.trip.validation.patchtime.TripPatchTimeConstraint;
+import toy.five.triprecord.global.exception.BaseException;
+import toy.five.triprecord.global.exception.ErrorCode;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@TripPatchTimeConstraint
+public class TripPatchRequest {
+
+
+    private String name;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Boolean isDomestic;
+
+
+    public void PatchFromTripPatchRequest(TripPatchRequest tripPatchRequest) {
+        if (!tripPatchRequest.getName().isEmpty()) {
+            this.name = tripPatchRequest.getName();
+        }
+        if (tripPatchRequest.getStartTime() != null) {
+            this.startTime = tripPatchRequest.getStartTime();
+        }
+        if (tripPatchRequest.getEndTime() != null) {
+            this.endTime = tripPatchRequest.getEndTime();
+        }
+        if (tripPatchRequest.getIsDomestic() != null) {
+            this.isDomestic = tripPatchRequest.getIsDomestic();
+        }
+    }
+
+
+
+}
