fe1ee0b75ed31b04c8680e6151a7b094e2683e62
parkgeonwoo <pgw111111@naver.com>
2023-10-26 12:24:51 +0900

    Refactor:validation 클래스 의존성 추가
    - validation 클래스 의존성 추가
    - 성공했을시 공통 API Response로 구현
    - 실패시 validation 메소드에서 BaseException을 발생시켜 해당 부분 처리하도록 구현

--- src/main/java/toy/five/triprecord/domain/trip/controller/TripController.java
+++ src/main/java/toy/five/triprecord/domain/trip/controller/TripController.java
@@ -3,6 +3,7 @@ package toy.five.triprecord.domain.trip.controller;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import toy.five.triprecord.domain.trip.dto.TripEntryResponse;
@@ -11,6 +12,8 @@ import toy.five.triprecord.domain.trip.dto.request.TripUpdateRequest;
 import toy.five.triprecord.domain.trip.dto.response.TripCreateResponse;
 import toy.five.triprecord.domain.trip.dto.response.TripUpdateResponse;
 import toy.five.triprecord.domain.trip.service.TripService;
+import toy.five.triprecord.domain.trip.validation.TripValidation;
+import toy.five.triprecord.global.exception.ApiResponse;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,6 +25,7 @@ import java.util.List;
 public class TripController {
 
     private final TripService tripService;
+    private final TripValidation tripValidation;
 
     @GetMapping("/{tripId}")
     public TripEntryResponse getTrip(@PathVariable final Long tripId) {
@@ -45,26 +49,34 @@ public class TripController {
      **/
 
     @PostMapping
-    public ResponseEntity<TripCreateResponse> createTrip(@RequestBody TripCreateRequest tripCreateRequest) {
+    public ResponseEntity<ApiResponse> createTrip(@RequestBody TripCreateRequest tripCreateRequest) {
+        tripValidation.validateNameDomesticTrip(tripCreateRequest);
+        tripValidation.validateTimeTrip(tripCreateRequest);
+
         TripCreateResponse savedTrip = tripService.createTrip(tripCreateRequest);
-        return ResponseEntity.ok(savedTrip);
-        //exceptionHandler로 처리 예정
+
+        return ResponseEntity.ok(ApiResponse.builder().status("Success").code(HttpStatus.OK.value()).data(savedTrip).build());
+
     }
 
     /**
      * 여행 수정 요청
      *
      * @param tripId {@link Long} 여행글 ID 요청 파라미터
-     * @param tripCreateRequest {@link TripCreateRequest} 여행 수정 내용 요청 파라미터
+     * @param tripUpdateRequest {@link TripCreateRequest} 여행 수정 내용 요청 파라미터
      * @return {@link ResponseEntity}
      **/
 
 
     @PutMapping("/{tripId}")
-    public ResponseEntity<TripUpdateResponse> updateTrip(@PathVariable Long tripId, @RequestBody TripUpdateRequest tripCreateRequest) {
-        TripUpdateResponse savedTrip = tripService.updateTrip(tripId,tripCreateRequest);
-        return ResponseEntity.ok(savedTrip);
-        //exceptionHandler로 처리 예정
+    public ResponseEntity<ApiResponse> updateTrip(@PathVariable Long tripId, @RequestBody TripUpdateRequest tripUpdateRequest) {
+        tripValidation.validateNameDomesticTrip(tripUpdateRequest);
+        tripValidation.validateTimeTrip(tripUpdateRequest);
+
+        TripUpdateResponse savedTrip = tripService.updateTrip(tripId,tripUpdateRequest);
+
+        return ResponseEntity.ok(ApiResponse.builder().status("Success").code(HttpStatus.OK.value()).data(savedTrip).build());
+
     }
 
     @PostConstruct
