d8d0c39a62fd6f57215eca52efe8f08ef3222405
parkgeonwoo <pgw111111@naver.com>
2023-10-27 01:12:52 +0900

    Refactor:기본 메시지 설정
    -@Valid 진행 중 발생하는 기본 오류 메시지 설정

--- src/main/java/toy/five/triprecord/domain/trip/dto/request/TripCreateRequest.java
+++ src/main/java/toy/five/triprecord/domain/trip/dto/request/TripCreateRequest.java
@@ -2,10 +2,14 @@ package toy.five.triprecord.domain.trip.dto.request;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import toy.five.triprecord.domain.trip.validation.createtime.TripCreateTimeConstraint;
 
 
 import java.time.LocalDateTime;
@@ -15,10 +19,23 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@TripCreateTimeConstraint
 public class TripCreateRequest {
+
+    @NotNull(message ="필드가 전부 채워져야 합니다.")
+    @NotEmpty(message ="필드가 전부 채워져야 합니다.")
+    @NotBlank(message ="필드가 전부 채워져야 합니다.")
     private String name;
+
+    @NotNull(message ="시작 시간이  필요합니다.")
     private LocalDateTime startTime;
+
+    @NotNull(message ="필드가 전부 채워져야 합니다.")
     private LocalDateTime endTime;
+
+    @NotNull(message ="필드가 전부 채워져야 합니다.")
     private Boolean isDomestic;
+
+
 
 }
